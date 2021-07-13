8278cc5ca15b6c06f2cb15fe9eaefe13811c6ba6
ChinaskiJr <robin.colombier@protonmail.com>
2021-07-13 21:56:45 +0200

    Create an user in the fixtures

--- symfony/src/DataFixtures/AppFixtures.php
+++ symfony/src/DataFixtures/AppFixtures.php
@@ -9,10 +9,19 @@ use App\Entity\PoliticalParty;
 use App\Entity\Primary;
 use App\Entity\Program;
 use App\Entity\Theme;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture {
+
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
 
     public function load(ObjectManager $manager) {
         // Political Party, primary and candidates
@@ -293,6 +302,12 @@ EOT
             }
             $manager->persist($glossary);
         }
+
+        $user = new User();
+        $user->setUsername('admin');
+        $user->setPassword($this->passwordHasher->hashPassword($user, '1231'));
+        $user->setRoles(['ROLE_ADMIN']);
+        $manager->persist($user);
 
         $manager->flush();
     }
