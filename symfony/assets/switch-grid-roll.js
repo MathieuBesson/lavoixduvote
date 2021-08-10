d051391b2fc362ef9a134e3d17e67ffcffb996d9
mathieu-besson <mathieubesson01@orange.fr>
2021-08-10 17:28:54 +0200

    Forgot the js file for the switcher

--- symfony/assets/switch-grid-roll.js
+++ symfony/assets/switch-grid-roll.js
@@ -1,14 +1,16 @@
 // Apparition of each part of home page with the switchers 
-const switcherGrid = document.querySelector('#switcher-grid');
-const switcherRoll = document.querySelector('#switcher-roll');
+const switcherGrid = document.querySelectorAll('.switcher-grid');
+const switcherRoll = document.querySelectorAll('.switcher-roll');
 
 const candidatesGridWrapper = document.querySelector('#candidates-grid-wrapper');
 const candidatesRollWrapper = document.querySelector('#candidates-roll-wrapper');
 
 function appearance(onClickElement, elementToAppear, elementToDisappear){
-    onClickElement.addEventListener('click', () => {
-        elementToAppear.classList.add('switcher-appearance');
-        elementToDisappear.classList.remove('switcher-appearance');
+    [...onClickElement].map(element => {
+        element.addEventListener('click', () => {
+            elementToAppear.classList.add('switcher-appearance');
+            elementToDisappear.classList.remove('switcher-appearance');
+        })
     })
 }
 
