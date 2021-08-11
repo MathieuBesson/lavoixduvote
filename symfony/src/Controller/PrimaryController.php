957c11046d0aa0e220573c86d7c822eb757c11d4
ChinaskiJr <robin.colombier@protonmail.com>
2021-08-11 21:44:18 +0200

    List the primaries in menu

---
+++ symfony/src/Controller/PrimaryController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Candidate;
+use App\Entity\Primary;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class PrimaryController
+ * @package App\Controller
+ *
+ * @Route (name="primaries", path="/primaires")
+ */
+class PrimaryController extends AbstractController
+{
+    /**
+     * @Route(name="_list_name", path="/list-name")
+     */
+    public function listPrimariesName(): Response
+    {
+        $primaries = $this->getDoctrine()
+            ->getRepository(Primary::class)
+            ->findAll();
+
+        return $this->render('primary/primary_list_name.html.twig', [
+            'primaries' => $primaries
+        ]);
+    }
+
+    /**
+     * @Route(name="choiceprimary", path="/choice/{primary}")
+     */
+    public function choicePrimary($primary) {
+
+    }
+}
--- symfony/src/Repository/PrimaryRepository.php
+++ symfony/src/Repository/PrimaryRepository.php
@@ -18,33 +18,4 @@ class PrimaryRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Primary::class);
     }
-
-    // /**
-    //  * @return Primary[] Returns an array of Primary objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('p.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Primary
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
