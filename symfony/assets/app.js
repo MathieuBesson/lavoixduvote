e36b0788118647f8d8f52c208f0da7e0a5e31594
ChinaskiJr <robin.colombier@protonmail.com>
2021-08-11 18:32:16 +0200

    Fix the overflow if star measures is on two lines

--- symfony/assets/app.js
+++ symfony/assets/app.js
@@ -5,6 +5,7 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 require('bootstrap/dist/js/bootstrap.bundle')
+require('@tannerhodges/match-height')
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
