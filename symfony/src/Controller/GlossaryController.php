70a46170f79c734baf50421ef01777d4906139b5
mathieu-besson <mathieubesson01@orange.fr>
2021-08-12 22:56:07 +0200

    Display the structure of glossary page

---
+++ symfony/src/Controller/GlossaryController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class GlossaryController
+ * @package App\Controller
+ *
+ * @Route (name="glossary", path="/jargon-politique")
+ */
+class GlossaryController extends AbstractController
+{
+    /**
+     * @Route(name="_index", path="/")
+     */
+    public function index(): Response
+    {
+        $repository = $this->getDoctrine()->getRepository('App\Entity\GlossaryCategory');
+        $glossaryCategories = $repository->findAll();
+
+        $repository = $this->getDoctrine()->getRepository('App\Entity\Glossary');
+        $definitions = $repository->findAll();
+
+        return $this->render('glossary/glossary_index.html.twig', [
+            'glossaryCategories' => $glossaryCategories,
+            'definitions' => $definitions
+        ]);
+    }
+}
