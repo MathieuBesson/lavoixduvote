7b3f65eef7ace7d68a670f01364730aebb09eb0c
ChinaskiJr <robin.colombier@protonmail.com>
2021-08-14 00:24:41 +0200

    Fix my mistake with migrations

--- symfony/migrations/Version20210813222332.php
+++ symfony/migrations/Version20210813222332.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210811194304 extends AbstractMigration
+final class Version20210813222332 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,13 +20,13 @@ final class Version20210811194304 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE political_party ALTER acronym SET NOT NULL');
+        $this->addSql('ALTER TABLE political_party ADD acronym VARCHAR(10) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('ALTER TABLE political_party ALTER acronym DROP NOT NULL');
+        $this->addSql('ALTER TABLE political_party DROP acronym');
     }
 }
