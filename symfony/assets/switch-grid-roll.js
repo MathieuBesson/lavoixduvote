050c4877203b1cc27149a989b829c11621452c64
ChinaskiJr <robin.colombier@protonmail.com>
2021-10-17 22:43:48 +0200

    [Markup Item #1] Only the active wrapper should have his icon in primary

--- symfony/assets/switch-grid-roll.js
+++ symfony/assets/switch-grid-roll.js
@@ -5,14 +5,21 @@ const switcherRoll = document.querySelectorAll('.switcher-roll');
 const candidatesGridWrapper = document.querySelector('#candidates-grid-wrapper');
 const candidatesRollWrapper = document.querySelector('#candidates-roll-wrapper');
 
-function appearance(onClickElement, elementToAppear, elementToDisappear){
-    [...onClickElement].map(element => {
-        element.addEventListener('click', () => {
-            elementToAppear.classList.add('switcher-appearance');
-            elementToDisappear.classList.remove('switcher-appearance');
-        })
-    })
+function appearance(onClickElement, elementToAppear, elementToDisappear) {
+	[...onClickElement].map(element => {
+		element.addEventListener('click', () => {
+			elementToAppear.classList.add('switcher-appearance');
+			elementToDisappear.classList.remove('switcher-appearance');
+			[...switcherGrid].map(element => {
+				element.classList.toggle('on');
+			});
+			[...switcherRoll].map(element => {
+				element.classList.toggle('on');
+			});
+		});
+	});
 }
+
 
 appearance(switcherGrid, candidatesGridWrapper, candidatesRollWrapper); 
 appearance(switcherRoll, candidatesRollWrapper, candidatesGridWrapper); 
