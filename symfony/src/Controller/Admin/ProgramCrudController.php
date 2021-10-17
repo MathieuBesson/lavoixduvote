f5aa9d05bf6b0bc019a13116d9563e24f1c33117
ChinaskiJr <robin.colombier@protonmail.com>
2021-10-17 11:37:10 +0200

    Dirty shits for dirty needs... Now the actions could be set on a program crud

--- symfony/src/Controller/Admin/ProgramCrudController.php
+++ symfony/src/Controller/Admin/ProgramCrudController.php
@@ -4,12 +4,14 @@ namespace App\Controller\Admin;
 
 use App\Entity\Program;
 use App\Form\ActionType;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\UrlField;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
 
 class ProgramCrudController extends AbstractCrudController
 {
@@ -21,18 +23,27 @@ class ProgramCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            AssociationField::new('candidate', 'Candidate'),
+            AssociationField::new('candidate', 'Candidate')->setCssClass('candidate-wrapper'),
             TextEditorField::new('presentation', 'Presentation'),
             UrlField::new('programLink', 'Program link'),
             CollectionField::new('actions', 'Actions')
-	            ->setSortable(true)
-		        ->allowAdd()
-		        ->allowDelete()
-		        ->setEntryIsComplex(true)
-		        ->setEntryType(ActionType::class)
-		        ->setFormTypeOptions([
-					'by_reference' => 'false'
-	        ])->setSortable(true)
+                ->setSortable(true)
+                ->allowAdd()
+                ->allowDelete()
+                ->setEntryIsComplex(true)
+                ->setEntryType(ActionType::class)
+                ->setFormTypeOptions([
+                    'by_reference' => 'true'
+                ])
+                ->setCssClass('actions-wrapper')
         ];
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        $crud
+            ->overrideTemplate('crud/new', 'admin/program/new.html.twig')
+            ->overrideTemplate('crud/edit', 'admin/program/edit.html.twig');
+        return $crud;
     }
 }
--- symfony/src/Form/ActionType.php
+++ symfony/src/Form/ActionType.php
@@ -5,6 +5,8 @@ namespace App\Form;
 use App\Entity\Action;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ActionType extends AbstractType
@@ -16,7 +18,10 @@ class ActionType extends AbstractType
             ->add('importance')
             ->add('theme')
             ->add('programs')
-        ;
+            ->addEventListener(FormEvents::PRE_SUBMIT, function (FormEvent $event) {
+                $program_id = $event->getForm()->getParent()->getParent()->getNormData()->getId();
+                $event->setData(array_merge($event->getData(), ['programs' => (string) $program_id]));
+            });
     }
 
     public function configureOptions(OptionsResolver $resolver)
