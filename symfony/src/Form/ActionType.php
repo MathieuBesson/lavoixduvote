48f569dcf433b91216eec83016716783037a96e1
ChinaskiJr <robin.colombier@protonmail.com>
2021-10-30 15:30:53 +0200

    Fix the bug that make actions not creatable on program crud

--- symfony/src/Form/ActionType.php
+++ symfony/src/Form/ActionType.php
@@ -20,7 +20,7 @@ class ActionType extends AbstractType
             ->add('programs')
             ->addEventListener(FormEvents::PRE_SUBMIT, function (FormEvent $event) {
                 $program_id = $event->getForm()->getParent()->getParent()->getNormData()->getId();
-                $event->setData(array_merge($event->getData(), ['programs' => (string) $program_id]));
+                $event->setData(array_merge($event->getData(), ['programs' => [(string) $program_id]]));
             });
     }
 
