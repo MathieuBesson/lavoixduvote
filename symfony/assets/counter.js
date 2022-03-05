57849823c00a14455e8c96f765eaf2157fd5c88f
mathieu-besson <mathieubesson01@orange.fr>
2022-03-05 21:29:38 +0100

    Fix bug on date passed

--- symfony/assets/counter.js
+++ symfony/assets/counter.js
@@ -4,14 +4,20 @@ class Counter {
     constructor(timesClass, dateEvent){
         this.durationBeforeEvent = new Date(dateEvent) - new Date();
         this.timerElements = Array.from(document.querySelectorAll(timesClass)).reverse();
+        this.intervalId = null; 
     }
 
     activation(){
         const vset = (e,t,c) => {
             const m = c ? t % c : t;
-            e.setAttribute('b', m < 10 ? '0' + m : m);
-            e.classList.remove('ping');
-            setTimeout(() => e.classList.add('ping'), 10);
+            if(this.durationBeforeEvent <= 0){
+                e.setAttribute('b', "00");
+            } else {
+                e.setAttribute('b', m < 10 ? '0' + m : m);
+                e.classList.remove('ping');
+                setTimeout(() => e.classList.add('ping'), 10);
+            }
+
             return m;
         };
         const calc = (t,i=0,b=0) => {
@@ -20,11 +26,25 @@ class Counter {
             if (vset(this.timerElements[i],t,Counter.s[i+1])==Counter.s[i+1]-1 || b) calc(t,i+1,b);
         }
         
-        const count = (b=0) => (this.durationBeforeEvent -= 1000) && calc(this.durationBeforeEvent,0,b);
+        const count = (b=0) => {
+            if(this.itsTime()){
+                return; 
+            }
+            return (this.durationBeforeEvent -= 1000) && calc(this.durationBeforeEvent,0,b);
+        }
         const opti = (v,n) => (v - (v % n)) / n;
         
-        setTimeout(() => !count(1) && setInterval(count, 1000), this.durationBeforeEvent % 1000);
+        setTimeout(() => {
+            this.intervalId = setInterval(count, 1000);
+            return !count(1) && this.intervalId
+        }, this.durationBeforeEvent % 1000);
     }
+
+    itsTime(){
+        if(this.durationBeforeEvent <= 0){
+            clearInterval(this.intervalId);
+        }
+    }   
 }
 
 let counterFirstRound = new Counter('.counter-first-round', '2022-04-10T20:00:00'); 
